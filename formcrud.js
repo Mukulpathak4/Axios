2db8151806b07610ed26155d7ec30b2935d81c88
Mukul pathak <mukulpathak4@gmail.com>
2023-07-18 12:16:50 +0530

    new update

--- formcrud.js
+++ formcrud.js
@@ -1,10 +1,16 @@
 window.onload = function () {
     // Retrieve data from local storage and display it on screen
     const storedData = JSON.parse(localStorage.getItem('PRODUCTS'));
+    
     if (storedData) {
+        let totalexpense = 0; // Initialize total expense
+        
         storedData.forEach(obj => {
             showUserOnScreen(obj);
+            totalexpense += parseInt(obj.price); // Add the price to total expense
         });
+        
+        showTotalExpense(totalexpense); // Display the total expense
     }
 }
 
@@ -37,34 +43,58 @@ window.addEventListener("DOMContentLoaded", () => {
                 console.log(response);
                 const data = response.data;
                 localStorage.setItem('Products', JSON.stringify(data));
+                let totalexpense = 0; // Initialize total expense
+                
                 data.forEach(obj => {
                     showUserOnScreen(obj);
+                    totalexpense += parseInt(obj.price); // Add the price to total expense
                 });
+                
+                showTotalExpense(totalexpense); // Display the total expense
             })
             .catch((err) => {
                 console.log(err);
             });
     }
 });
-
 function showUserOnScreen(obj) {
-    const parentElement = document.getElementById('listOfItems')
+    const parentElement = document.getElementById('listOfItems');
     const childItem = document.createElement('li');
-    childItem.textContent = obj.price + '-' + obj.productname
+    childItem.textContent = obj.price + '-' + obj.productname;
 
     const deletebtn = document.createElement('input');
-    deletebtn.type = 'button'
-    deletebtn.value = 'Delete'
+    deletebtn.type = 'button';
+    deletebtn.value = 'Delete';
     deletebtn.onclick = () => {
         axios.delete(`https://crudcrud.com/api/973d55282fdc470492d1162af976e1df/PRODUCTS/${obj._id}`)
             .then((response) => {
                 console.log(response.data);
                 parentElement.removeChild(childItem);
+                
+                // Update the total expense after deletion
+                let totalexpense = parseInt(document.getElementById('expense').textContent);
+                totalexpense -= parseInt(obj.price);
+                showTotalExpense(totalexpense);
             })
             .catch((err) => {
                 console.log(err);
             });
-    }
+    };
     childItem.appendChild(deletebtn);
     parentElement.appendChild(childItem);
+
+    // Update the total expense when a new item is added
+    let totalexpense = parseInt(document.getElementById('expense').textContent);
+    totalexpense += parseInt(obj.price);
+    showTotalExpense(totalexpense);
 }
+function showTotalExpense(expense) {
+    const expenseElement = document.getElementById('expense');
+    expenseElement.textContent = expense;
+}
+
+
+
+
+
+
